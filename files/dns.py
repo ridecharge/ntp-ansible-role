16058cbdce4d42ed5194a539882088556eb05f41
Stephen Garlick <sgarlick@taximagic.com>
2015-01-13 20:23:12 -0500

    fix script

--- files/dns.py
+++ files/dns.py
@@ -7,10 +7,10 @@ ip = boto.utils.get_instance_metadata()['local-ipv4']
 region = boto.utils.get_instance_identity()['document']['region']
 conn = boto.route53.connect_to_region(region)
 
-instance_type = sys.argv[1].lower()
+instance_type = sys.argv[2].lower()
 domain_az = boto.utils.get_instance_metadata()['placement']['availability-zone'].split('-')[-1]
 
-hosted_zone = sys.argv[0]
+hosted_zone = sys.argv[1]
 domain = instance_type+"-"+domain_az+".ec2.gc"
 
 changes = boto.route53.record.ResourceRecordSets(conn, hosted_zone)
