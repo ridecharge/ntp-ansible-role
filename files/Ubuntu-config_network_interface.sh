d612302b6bf13460d1b1e15b7ac2610133ad91f2
Stephen Garlick <sgarlick@taximagic.com>
2015-02-02 17:30:40 -0500

    setup eth1 script

---
+++ files/Ubuntu-config_network_interface.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ETH=$1
+while [ -z "$(ifconfig -a | grep $ETH)" ]; do sleep 3s; done;
+cat /etc/network/interfaces.d/eth0.cfg | sed 's/eth0/$ETH/g' > /etc/network/interfaces.d/$ETH.cfg
+ifup $ETH
+echo "200 out" >> /etc/iproute2/rt_tables
+GATEWAY=$(route -n | awk 'FNR == 3 {print $2}')
+PRIVATE_IP=$(ifconfig -a | grep -A 1 $ETH | awk 'FNR == 2 { print $2 }' | sed 's/.*://')
+ip route add default via $GATEWAY dev $ETH table out
+ip rule add from $PRIVATE_IP/32 table out
+ip rule add to $PRIVATE_IP/32 table out
+ip route flush cache
